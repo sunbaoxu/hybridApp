f515f86f193f28a42d180a68c3865bfd25510da2
sunbaoxu <1194063234@qq.com>
2018-04-18 18:03:27 +0800

    更改打包路径

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../h5-app'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/h5-app',
 
     /**
      * Source Maps
