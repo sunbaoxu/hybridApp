22198251111876522873fee5eb4e70d48932065f
sunbaoxu <1194063234@qq.com>
2018-04-19 11:41:38 +0800

    新增bank

---
+++ src/components/bank/ChangeBank.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bank-wrap" >
+    dsfasdfdasf
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+export default {
+  name: 'bank',
+  data () {
+    return {
+      allAsync : false
+    }
+  },
+  methods : {
+    
+  }
+}
+</script>
+<style lang="scss" scoped>
+.bank-wrap{
+
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,9 @@ const ShangList            = resolve => require(['@/components/business/ShangLis
 //商家列表
 const ShangDetail           = resolve => require(['@/components/business/ShangDetail.vue'], resolve);
 
+
+
+
 //上传照片
 const UploadeImg    = resolve => require(['@/components/order/UploadeImg.vue'], resolve);
 
@@ -16,6 +19,8 @@ const LoanBaocun    = resolve => require(['@/components/order/LoanBaocun.vue'], 
 //协议页
 const Agreement    = resolve => require(['@/components/order/state/Agreement.vue'], resolve);
 
+
+
 //订单确认
 const ConfirmOrder    = resolve => require(['@/components/order/ConfirmOrder.vue'], resolve);
 //订单状态
@@ -24,38 +29,53 @@ const OrderState    = resolve => require(['@/components/order/state/OrderState.v
 const OrderRecord    = resolve => require(['@/components/order/state/OrderRecord.vue'], resolve);
 //还款详情
 const stillDetail    = resolve => require(['@/components/order/details/stillDetail.vue'], resolve);
+
+
+
+
+//还款详情
+const ChangeBank    = resolve => require(['@/components/bank/ChangeBank.vue'], resolve);
+
+
 Vue.use(Router);
 export default new Router({
   mode :'history',
   routes: [
+    //商家
     //商家推荐码
-    {path: '/shangCode',name: 'shangCode',component: ShangCode},
+    {path: '/shangCode',name: '商家推荐码',component: ShangCode},
     //商家列表
-    {path: '/shangList',name: 'shangList',component: ShangList},
+    {path: '/shangList',name: '商家列表',component: ShangList},
     //商家详情
-    {path: '/shangDetail',name: 'shangDetail',component: ShangDetail},
-    //上传照片
-    {path: '/uploadeImg',name: 'uploadeImg',component: UploadeImg},
+    {path: '/shangDetail',name: '商家详情',component: ShangDetail},
 
+
+    //订单
     //确认订单
-    {path: '/confirmOrder',name: 'confirmOrder',component: ConfirmOrder},
+    {path: '/confirmOrder',name: '确认订单',component: ConfirmOrder},
     //订单状态
-    {path: '/orderState/',name: 'orderState',component: OrderState},
+    {path: '/orderState',name: '订单状态',component: OrderState},
     //订单详情
-    {path: '/stillDetail',name: 'stillDetail',component: stillDetail},
-
+    {path: '/stillDetail',name: '订单详情',component: stillDetail},
     //还款记录
-    {path: '/orderRecord',name: 'orderRecord',component: OrderRecord},
+    {path: '/orderRecord',name: '还款记录',component: OrderRecord},
 
+
+    //银行卡
+    {path: '/bank/changeBank',name: '银行卡列表',component: ChangeBank},
 
 
     //保存借款信息
     {path: '/loanBaocun',name: 'loanBaocun',component: LoanBaocun},
     //协议页
     {path: '/agreement',name: 'agreement',component: Agreement},
+    //上传照片
+    {path: '/uploadeImg',name: '上传照片',component: UploadeImg},
 
 
-    //协议页
+
+
+    //404
     {path: '*',name: '404',component: ShangCode},
   ]
 });
